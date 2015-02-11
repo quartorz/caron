51df3b6af2ddfb116a533c6ae1d4e5d2072f6422
quartorz <tsuhaso@gmail.com>
2015-02-11 21:36:14 +0900

    add test

---
+++ test/test.py
@@ -0,0 +1,20 @@
+preamble = {
+    'title': 'Test',
+    'author': 'quarter',
+}
+
+with Document():
+    make_title()
+
+    with Section('section'):
+        with SubSection('sub section 1'):
+            with Environment('table', optional='htbp'):
+                centering()
+                caption('table test', label='table')
+                with Environment('tabular', required='cc'):
+                    line += 'a & b \\\\'
+                    line += 'c & d '
+
+        with SubSection('sub section 2'):
+            with SourceCode('test.py'):
+                pass
